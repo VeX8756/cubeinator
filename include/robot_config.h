6865feaa3ba3024160b07b88ee802bab07c99469
VeX8756 <mccallum.robotics@gmail.com>
2019-12-15 15:58:35 -0600

    Update robot_config.h

--- include/robot_config.h
+++ include/robot_config.h
@@ -5,7 +5,7 @@ vex::motor FrontLeftMotor(vex::PORT20, vex::gearSetting::ratio18_1, false);
 vex::motor FrontRightMotor(vex::PORT11, vex::gearSetting::ratio18_1, false);
 vex::motor BackLeftMotor(vex::PORT10, vex::gearSetting::ratio18_1, false);
 vex::motor BackRightMotor(vex::PORT1, vex::gearSetting::ratio18_1, false);
-vex::motor LiftMotor1(vex::PORT7, vex::gearSetting::ratio18_1, false);
+vex::motor LiftMotor1(vex::PORT7, vex::gearSetting::ratio18_1, false);  // red cartriges are 6_1
 vex::motor LiftMotor2(vex::PORT9, vex::gearSetting::ratio18_1, true);
 vex::motor GrabMotor(vex::PORT12, vex::gearSetting::ratio18_1, false);
 vex::controller Controller1;
